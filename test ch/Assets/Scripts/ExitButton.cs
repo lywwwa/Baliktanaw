1c2916d0947640328a16ce58a8fcec444b8250d8
Lyra Danielle Tablico <lyra_dainel@yahoo.com>
2018-11-24 15:28:48 +0900

    Loading screen added colliders fixed

---
+++ test ch/Assets/Scripts/ExitButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ExitButton : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ExitPressed(){
+		Application.Quit ();
+
+	}
+
+	public void StartGame(){
+		SceneManager.LoadScene("Barangay");
+	}
+
+
+}
---
+++ test ch/Assets/Scripts/SelectionController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class SelectionController : MonoBehaviour {
+	
+	public EventSystem eventSystem;
+	public GameObject selectedObject;
+
+	private bool buttonSelected;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetAxis ("Vertical") != 0 && buttonSelected == false) 
+		{
+			eventSystem.SetSelectedGameObject(selectedObject);
+			buttonSelected = true;
+		}
+
+		if (Input.GetButton("Submit")) {
+			Debug.Log ("b button clicked");
+
+		}
+	}
+
+	private void OnDisable()
+	{
+		buttonSelected = false;
+	}
+
+	private void OnSelected()
+	{
+		
+	}
+
+}
