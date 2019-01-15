ec0cbca2f8b99c5d44a5836f984b3bdade2d37c0
Gherico <ihateeveryoneofthem@gmail.com>
2019-01-15 21:31:11 +0800

    Raycasting

---
+++ test ch/Assets/Scripts/GrabCasting.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabCasting : MonoBehaviour {
+
+    Transform playerLocation;
+    float xDistance = 3;
+    float yDistance = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        RaycastHit hit;
+
+        var fwd = transform.TransformDirection(Vector3.forward);
+        if(Physics.Raycast(transform.position,  fwd, out hit, 500))
+        {
+            Debug.Log(hit.collider.gameObject.tag);
+            
+            if(hit.collider.gameObject.tag == "Grass")
+            {
+                if (Input.GetButton("AButton"))
+                {
+                    hit.transform.SetParent(this.transform);
+                    hit.transform.localPosition = new Vector3(0f, -.672f, 0f);
+                }
+            }
+        }
+        
+
+    }
+}
