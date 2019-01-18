e6cf6bc7e366328b98bdebb09aa38a58d1dffe5a
JaimeDia <44525513+JaimeDia@users.noreply.github.com>
2019-01-18 23:06:20 +0800

    Sounds

---
+++ test ch/Assets/Scripts/FadeOut.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeOut : MonoBehaviour {
+
+	public GameObject title;
+
+	//SpriteRenderer rend;
+
+	void Start ()
+	{
+		//rend = GetComponent<SpriteRenderer>();
+		//startFading();
+		StartCoroutine(cone1UP(10f));
+	}
+
+	/*IEnumerator Fade()
+	{
+		for (float f = 1f; f>= -0.05f; f -= 0.05f)
+		{
+			Color c = rend.material.color;
+			c.a = f;
+			rend.material.color = c;
+			yield return new WaitForSeconds (0.05f);
+		}
+	}
+
+	public void startFading()
+	{
+		StartCoroutine("Fade");
+	}*/
+
+	IEnumerator cone1UP(float seconds)
+	{
+		yield return new WaitForSeconds(seconds);
+		//title.a = 0.42f;
+		//title.alpha = "0";
+		//cone1.transform.Translate(0, 400, 0);
+	}
+}
+
+
